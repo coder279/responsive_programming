734b211141431f8ac9d76d92ac329a39580c52a9
coder279 <coder279@126.com>
2022-07-08 16:12:01 +0800

    :sparkles: feat: Added comparison between multi-threaded code and lambda expressions before java8
    Signed-off-by: coder279 <coder279@126.com>

--- src/com/example/thread/ThreadDemo.java
+++ src/com/example/thread/ThreadDemo.java
@@ -1,5 +1,10 @@
 package com.example.thread;
 
+/**
+ * Author: Leo Brown
+ * Description: JDK8之前多线程代码与JDK8以后多线程代码对比(Lamda表达式的好处)
+ * Date: 2022-07-08 16:11:33
+ */
 public class ThreadDemo {
     public static void main(String[] args) {
         //JDK8以前
